8a8cc15fe5453c1e30fc4ac2e690dc2e588e387e
HardlyDifficult <hardlydiff@gmail.com>
2025-07-17 14:13:39 -0400

    getTransactionTreeById readAs (#40)

--- src/clients/ledger-json-api/operations/v2/updates/get-transaction-tree-by-id.ts
+++ src/clients/ledger-json-api/operations/v2/updates/get-transaction-tree-by-id.ts
@@ -7,20 +7,55 @@ const endpoint = '/v2/updates/transaction-tree-by-id/{update-id}';
 export const GetTransactionTreeByIdParamsSchema = z.object({
   updateId: z.string(),
   verbose: z.boolean().optional(),
+  parties: z.array(z.string()).optional(),
 });
 
 export type GetTransactionTreeByIdParams = z.infer<typeof GetTransactionTreeByIdParamsSchema>;
 export type GetTransactionTreeByIdResponse = paths[typeof endpoint]['get']['responses']['200']['content']['application/json'];
 
+/**
+ * @description Retrieves transaction tree data by update ID
+ * @example
+ * ```typescript
+ * const transactionTree = await client.getTransactionTreeById({
+ *   updateId: '1220d01cce13fa797bad7de3ea61e919b20dce545d41f05e026ec4b65f1a61c71215',
+ *   parties: ['party1', 'party2']
+ * });
+ * ```
+ * @param updateId - The update ID to retrieve transaction tree for
+ * @param parties - Optional array of party IDs to filter transactions by
+ */
 export const GetTransactionTreeById = createApiOperation<
   GetTransactionTreeByIdParams,
   GetTransactionTreeByIdResponse
 >({
   paramsSchema: GetTransactionTreeByIdParamsSchema,
   method: 'GET',
-  buildUrl: (params, apiUrl) => {
-    const url = new URL(`${apiUrl}${endpoint.replace('{update-id}', params.updateId)}`);
-    if (params.verbose) url.searchParams.set('verbose', params.verbose.toString());
-    return url.toString();
+  buildUrl: (params, apiUrl, client) => {
+    const currentPartyId = client.getPartyId();
+    
+    const readParties = Array.from(
+      new Set([
+        currentPartyId,
+        ...(params.parties || []),
+      ])
+    );
+
+    const baseUrl = `${apiUrl}${endpoint.replace('{update-id}', params.updateId)}`;
+    
+    const queryParams = new URLSearchParams();
+    
+    if (readParties.length > 0) {
+      readParties.forEach(party => {
+        queryParams.append('parties', party);
+      });
+    }
+    
+    if (params.verbose) {
+      queryParams.set('verbose', params.verbose.toString());
+    }
+    
+    const queryString = queryParams.toString();
+    return queryString ? `${baseUrl}?${queryString}` : baseUrl;
   },
 }); 
