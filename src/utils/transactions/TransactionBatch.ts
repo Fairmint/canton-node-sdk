f5e4e6b45c172d8388a7b8d162188a930195a10b
HardlyDifficult <hardlydiff@gmail.com>
2025-10-20 21:51:24 -0400

    Async submit batch

--- src/utils/transactions/TransactionBatch.ts
+++ src/utils/transactions/TransactionBatch.ts
@@ -1,10 +1,6 @@
 import { type LedgerJsonApiClient } from '../../clients';
 import { type Command, type DisclosedContract } from '../../clients/ledger-json-api/schemas';
 
-export interface BatchSubmitResult {
-  updateId: string;
-}
-
 export class TransactionBatch {
   private readonly client: LedgerJsonApiClient;
   private readonly actAs: string[];
@@ -47,18 +43,34 @@ export class TransactionBatch {
     return this;
   }
 
-  public async submit(): Promise<BatchSubmitResult> {
+  private prepareSubmitParams() {
     // Dedupe disclosed contracts by contractId
     this.disclosedContracts = Array.from(
       new Map(this.disclosedContracts.map((contract) => [contract.contractId, contract])).values()
     );
-    const response = await this.client.submitAndWaitForTransactionTree({
+
+    return {
       actAs: this.actAs,
       ...(this.readAs ? { readAs: this.readAs } : {}),
       commands: this.commands,
       ...(this.disclosedContracts.length > 0 ? { disclosedContracts: this.disclosedContracts } : {}),
-    });
+    };
+  }
 
+  public async submitAndWaitForTransactionTree(): Promise<{ updateId: string }> {
+    const submitParams = this.prepareSubmitParams();
+    const response = await this.client.submitAndWaitForTransactionTree(submitParams);
     return { updateId: response.transactionTree.updateId };
+  }
+
+  public async submitAndWait(): Promise<{ updateId: string }> {
+    const submitParams = this.prepareSubmitParams();
+    const response = await this.client.submitAndWait(submitParams);
+    return { updateId: response.updateId };
+  }
+
+  public async asyncSubmit(): Promise<void> {
+    const submitParams = this.prepareSubmitParams();
+    await this.client.asyncSubmit(submitParams);
   }
 }
